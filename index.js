febf2681203989c6106a7045e0a0327f6c9cf236
Maxim Toma <maxim.toma@sky.uk>
2023-10-31 15:22:57 +0000

    Finished 'getImage' function that make an API call and get a link to a GIF'

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ require("dotenv").config();
 
 const { myAPI } = require("./apiHere.js");
 
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${myAPI}&q=pokemon&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+//const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${myAPI}&q=pokemon&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
-async function getGit() {
-  return;
+async function getImage(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${myAPI}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const response = await fetch(endpoint);
+  const dataRes = await response.json();
+
+  const url = dataRes.data[0].url;
+  console.log(url);
 }
-
+getImage("Minecraft");
 // Print out value of API key stored in .env file
 // It works but you just need 'npm install dotenv'
 console.log(process.env.API_KEY);
