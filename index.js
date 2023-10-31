525eaf676b8debe8da10b59178d45dce6178a42a
Maxim Toma <maxim.toma@sky.uk>
2023-10-31 15:02:35 +0000

    Imported API_KEY from another file because '.env' wasn't working

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
+//require("dotenv").config();
 
+//import { myAPI } from "./apiHere.js";
+
+const { myAPI } = require("./apiHere.js");
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${myAPI}&q=pokemon&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getGit() {
+  return;
+}
+console.log(myAPI);
+console.log(endpoint);
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
